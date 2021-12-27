098a07988e40f2aaac4d4e96d159a878dadcb2c3
BahiaRT <msimoes@uneb.br>
2021-12-27 20:54:59 -0300

    Atualizados run.sh e setup.sh para suportar NVIDIA e MESA. 	modified:   run.sh 	modified:   setup.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+GFX=$1
+USAGE="Usage: run.sh mesa|nvidia"
+if [ "x$GFX" = "x" ]; then
+    echo "$USAGE"
+    exit 1
+fi
+
+if [ "$GFX" = "nvidia" ]; then
+    RUNTIME="--gpus all"
+elif [ "$GFX" = "mesa" ]; then
+    RUNTIME=
+else
+    echo "$USAGE"
+    exit 1
+fi
+
+
 xhost +"local:docker@"
-docker run -it --volume=/dev:/dev --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw --volume=/home/$USER/.Xauthority:/root/.Xauthority:rw --volume=$PWD/setplays:/setplays:rw --env="XAUTHORITY=/root/.Xauthority" --env="DISPLAY=${DISPLAY}" --env="QT_X11_NO_MITSHM=1" --rm --name spds bahiart/spdataset:mesa-latest
+docker run -it $RUNTIME --volume=/dev:/dev --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw --volume=/home/$USER/.Xauthority:/root/.Xauthority:rw --volume=$PWD/setplays:/setplays:rw --env="XAUTHORITY=/root/.Xauthority" --env="DISPLAY=${DISPLAY}" --env="QT_X11_NO_MITSHM=1" --rm --name spds bahiart/spdataset:$GFX-latest
--- setup.sh
+++ setup.sh
@@ -1,2 +1,24 @@
-docker pull bahiart/spdataset:mesa-latest
+#!/bin/bash
+GFX=$1
+USAGE="Usage: setup.sh mesa|nvidia"
+if [ "x$GFX" = "x" ]; then
+    echo "$USAGE"
+    exit 1
+fi
+
+if [ "$GFX" = "nvidia" ]; then
+   distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+   sudo apt-get update
+   sudo apt-get install -y nvidia-docker2
+elif [ "$GFX" = "mesa" ]; then
+    distribution=
+else
+    echo "$USAGE"
+    exit 1
+fi
+
+
+docker pull bahiart/spdataset:$GFX-latest
 
